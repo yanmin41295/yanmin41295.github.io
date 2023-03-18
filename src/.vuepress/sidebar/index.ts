b2b6ef4fbb0530aa6c7f4b90e6de8139bcf4e952
yanmin41295 <yanmin41295@outlook.com>
2023-03-18 21:39:02 +0800

    remove english

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -1,2 +1,28 @@
-export * from "./en.js";
-export * from "./zh.js";
+import {sidebar} from "vuepress-theme-hope";
+
+export const zhSidebar = sidebar({
+    "/": [
+        "",
+        "intro",
+        {
+            text: "拉不拉东算法笔记",
+            icon: "note",
+            prefix: "labuladong/",
+            children: "structure",
+        },
+        /* {
+             text: "文章",
+             icon: "note",
+             prefix: "posts/",
+             children: "structure",
+         },*/
+        {
+            text: "如何使用",
+            icon: "creative",
+            prefix: "demo/",
+            link: "demo/",
+            children: "structure",
+        },
+        "slides",
+    ],
+});
